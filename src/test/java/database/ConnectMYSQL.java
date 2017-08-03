4cfcfc0219d6fd32b0d07c2ebcd07487aa3716cf
komalh <komalh@ULTP_649.synerzipune.local>
2017-08-03 18:08:55 +0530

    adding

--- src/test/java/database/ConnectMYSQL.java
+++ src/test/java/database/ConnectMYSQL.java
@@ -8,12 +8,29 @@ import java.sql.Statement;
 
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 
 
 public class ConnectMYSQL {
 	
+	ExtentReports report;
+	ExtentTest logger;
+	
 	@Test
 	public void testDB() throws ClassNotFoundException, SQLException{
+		
+
+		//Create object for Report with filepath
+		report=new ExtentReports("target/dbreport.html");
+		
+		//Start the test
+		logger=report.startTest("verifyDBdeatils");
+	
+		//Log the status in report
+		logger.log(LogStatus.INFO, "verifyDBdeatils ");
 		
 		 Class.forName("com.mysql.jdbc.Driver");
 	     System.out.println("Driver Loaded");
@@ -38,5 +55,14 @@ Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/selenium
 	             System.out.println("Uname is "+uname+" email id is "+email);
 	        }
 
+	        
+	      //Pass the test in report
+			logger.log(LogStatus.PASS, "Test Verified");
+			
+			//End the test
+			report.endTest(logger);
+			
+			//Flush the data to report
+			report.flush();   
 }
 }
